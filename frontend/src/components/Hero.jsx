64aeb4451dcdd6ae8ff920bdb91fea1a2a04dafd
Katrina Dawap <kattydee3@gmail.com>
2026-06-06 16:54:14 +0100

    added working links to nav

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -13,9 +13,9 @@ function Hero() {
                 <span>Showcase your talent, vote for the best, and celebrate Nigerian culture on Africa's biggest digital platform.</span>
             </div>
             <div className="hero-btns">
-                <button> <Link to="/upload">Upload Talent</Link></button>
-                <button> <Link to="/vote">Vote Now</Link></button>
-                <button> <Link to="/quiz">Join Quiz</Link></button>
+                <Link to="/upload"><button> Upload Talent</button></Link>
+                <Link to="/voting"><button>Vote Now</button></Link>
+                <Link to="/quiz"><button>Join Quiz</button></Link>
             </div>
         </div>
   )
